c167f9e1313ce9bdc1d4ae0ded125ac6c7164cbc
cathuan <geli7630@gmail.com>
2018-01-06 19:30:17 +1100

    solve q279.py

--- q279.py
+++ q279.py
@@ -2,24 +2,42 @@ import numpy as np
 import sys
 
 
+class PerfectSquares(object):
+
+    def __init__(self):
+        self.perfectSquares = [1]
+        self.current_max = 1
+
+    def request(self, n):
+        if n < (self.current_max + 1) ** 2:
+            return self.perfectSquares
+        else:
+            self.current_max += 1
+            self.perfectSquares.append(self.current_max ** 2)
+            return self.perfectSquares
+
+
+# 62.15%. Not good, apparantly.
 class Solution(object):
 
-    def getPerfectSquareNums(self, n):
-        perfect_square_nums = []
-        max_sqrt = int(np.sqrt(n))
-        for i in range(1, max_sqrt+1):
-            perfect_square_nums.append(i**2)
-        return perfect_square_nums
-
+    @profile
     def numSquares(self, n):
         """
         :type n: int
         :rtype: int
         """
 
+        perfect_squares_getter = PerfectSquares()
+        solved = [0]
+
+        for val in range(1,n+1):
+            perfect_squares = perfect_squares_getter.request(val)
+            solved.append(min(solved[-p] for p in perfect_squares)+1)
+
+        return solved[n]
+
 
 if __name__ == "__main__":
 
     n = int(sys.argv[1])
-    solution = Solution()
-    print solution.numSquares(n)
+    print Solution().numSquares(n)
