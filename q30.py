77bcaa6a449d9d98106f60e91ccfc1d668973006
cathuan <geli7630@gmail.com>
2018-06-22 00:47:32 +1000

    Finish q30 with bad performance

--- q30.py
+++ q30.py
@@ -16,10 +16,10 @@ class Solution(object):
         word_length = len(words[0])
         word_counter = {}
         for word in words:
-        	if word not in word_counter:
-        		word_counter[word] = 1
-        	else:
-        		word_counter[word] += 1
+            if word not in word_counter:
+                word_counter[word] = 1
+            else:
+                word_counter[word] += 1
 
         # set to store start index which is not a word
         not_word_indices = set()
@@ -27,24 +27,31 @@ class Solution(object):
         result = []
         
         for i in range(len(s) - len(words) * word_length+1):
-        	seen_words = {}
-        	for j in range(len(words)):
-        		curr = i + j * word_length
-        		if curr in not_word_indices:
-        			break
-	        	potential_word = s[curr, curr + word_length]
-    	    	if potential_word not in words_set:
-        			not_word_indices.add(curr)
-        			break
-        		else:
-        			if potential_word in seen_words:
-        				seen_words[potential_word] += 1
-        			else:
-        				seen_words[potential_word] = 1
-        	else:
-        		for word in word_counter:
-        			if word not in seen_words or seen_words[word] != word_counter[word]:
-        				break
-        		else:
-        			result.append(i)
+            seen_words = {}
+            for j in range(len(words)):
+                curr = i + j * word_length
+                if curr in not_word_indices:
+                    break
+
+                potential_word = s[curr:curr + word_length]
+                if potential_word not in words_set:
+                    not_word_indices.add(curr)
+                    break
+                if potential_word in seen_words:
+                    seen_words[potential_word] += 1
+                else:
+                    seen_words[potential_word] = 1
+            else:
+                for word in word_counter:
+                    if word not in seen_words or seen_words[word] != word_counter[word]:
+                        break
+                else:
+                    result.append(i)
         return result
+
+
+if __name__ == "__main__":
+
+	s = "barfoothefoobarman"
+	words = ["foo","bar"]
+	print Solution().findSubstring(s, words)
