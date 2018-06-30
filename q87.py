732f0117dc3fb99736514b3c969c11d6284f5d40
cathuan <geli7630@gmail.com>
2018-06-30 16:02:12 +1000

    Finish q87

--- q87.py
+++ q87.py
@@ -2,76 +2,70 @@ import sys
 
 
 class Solution(object):
-    def isScramble(self, s1, s2):
-        """
-        :type s1: str
-        :type s2: str
-        :rtype: bool
-        """
-        
-        if s1 == s2:
-            return True
-        
-        if len(s1) != len(s2):
-            return False
-        
-        letters = {}
-        seenPartition = False
-        isAnalogue = False
 
-        for i in range(len(s1)):
-            if isAnalogue:
-                seenPartition = True
+	def isScramble(self, s1, s2):
+		"""
+		:type s1: str
+		:type s2: str
+		:rtype: bool
+		"""
+		
+		if s1 == s2:
+			return True
+		
+		if len(s1) != len(s2):
+			return False
+		
+		letters = {}
+		for i in range(len(s1)-1):
 
-            char1 = s1[i]
-            char2 = s2[i]
-            if char1 not in letters:
-                letters[char1] = 0
-            if char2 not in letters:
-                letters[char2] = 0
-            letters[char1] += 1
-            letters[char2] -= 1
-            
-            isAnalogue = True
-            for char in letters:
-                if letters[char] < 0:
-                    isAnalogue = False
-                    break
-            else:
-                isAnalogue = True
-            
-        sameDirection = seenPartition and isAnalogue
+			char1 = s1[i]
+			char2 = s2[i]
+			if char1 not in letters:
+				letters[char1] = 0
+			if char2 not in letters:
+				letters[char2] = 0
+			letters[char1] += 1
+			letters[char2] -= 1
+			
+			for char in letters:
+				if letters[char] < 0:
+					isAnalogue = False
+					break
+			else:
+				isAnalogue = True
 
-        letters = {}
-        seenPartition = False
-        isAnalogue = False
+			if isAnalogue:
+				if self.isScramble(s1[:i+1], s2[:i+1]) and self.isScramble(s1[i+1:], s2[i+1:]):
+					return True
 
-        for i in range(len(s1)):
-            if isAnalogue:
-                seenPartition = True
+		s2 = s2[::-1]  # revert the string
+		letters = {}
+		for i in range(len(s1)-1):
 
-            char1 = s1[i]
-            char2 = s2[-1-i]
-            if char1 not in letters:
-                letters[char1] = 0
-            if char2 not in letters:
-                letters[char2] = 0
-            letters[char1] += 1
-            letters[char2] -= 1
-            
-            isAnalogue = True
-            for char in letters:
-                if letters[char] < 0:
-                    isAnalogue = False
-                    break
-            else:
-                isAnalogue = True
-            
-        oppoDirection = seenPartition and isAnalogue
+			char1 = s1[i]
+			char2 = s2[i]
+			if char1 not in letters:
+				letters[char1] = 0
+			if char2 not in letters:
+				letters[char2] = 0
+			letters[char1] += 1
+			letters[char2] -= 1
+			
+			for char in letters:
+				if letters[char] < 0:
+					isAnalogue = False
+					break
+			else:
+				isAnalogue = True
 
-        return sameDirection or oppoDirection
+			if isAnalogue:
+				if self.isScramble(s1[:i+1], s2[:i+1]) and self.isScramble(s1[i+1:], s2[i+1:]):
+					return True
+
+		return False
 
 
 if __name__ == '__main__':
 
-    print Solution().isScramble(sys.argv[1], sys.argv[2])
+	print Solution().isScramble(sys.argv[1], sys.argv[2])
