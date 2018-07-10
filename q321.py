deb179ba487620ffb0e6cf8aa52f8f0fcf1de3c3
cathuan <geli7630@gmail.com>
2018-07-11 00:25:40 +1000

    Finish q321

--- q321.py
+++ q321.py
@@ -1,6 +1,5 @@
 class Solution(object):
 
-	@profile
 	def maxNumber(self, nums1, nums2, k):
 		"""
 		:type nums1: List[int]
@@ -9,77 +8,40 @@ class Solution(object):
 		:rtype: List[int]
 		"""
 
-		dp = {}
+		result = []
+		for n1 in range(max(k-len(nums2), 0), min(len(nums1)+1, k+1)):
+			n2 = k - n1
+			r1 = self.findMaxArray(nums1, n1)
+			r2 = self.findMaxArray(nums2, n2)
+			r = self.merge(r1,r2)
+			result = max(result, r)
+		return result
 
-		stack = []
-		stack.append((len(nums1), len(nums2), k))
-		while len(stack) > 0:
+	def findMaxArray(self, nums, n):
 
-			if stack[-1] in dp:
-				stack.pop()
-				continue
-			n1, n2, k_ = stack[-1]
-			#print n1, n2, k_, stack[-1]
+		drop = len(nums) - n
+		result = []
+		for num in nums:
+			while drop > 0 and (len(result) > 0 and result[-1] < num):
+				result.pop()
+				drop -= 1
+			result.append(num)
+		return result[:n]
 
-			if k_ == 0:
-				dp[(n1, n2, k_)] = []
-				stack.pop()
-				continue
-
-			nums1_ = nums1[-n1:] if n1 != 0 else []
-			nums2_ = nums2[-n2:] if n2 != 0 else []
-
-			length = len(nums1_) + len(nums2_) - k + 1
-			maxIndex1 = self.findMax(nums1_, length)
-			maxIndex2 = self.findMax(nums2_, length)
-			#print "***", maxIndex1, maxIndex2, len(nums1_), len(nums2_)
-
-			maxValue1 = nums1_[maxIndex1] if maxIndex1 is not None else float("-inf")
-			maxValue2 = nums2_[maxIndex2] if maxIndex2 is not None else float("-inf")
-
-			if maxValue1 == maxValue2:
-				needContinue = False
-				if (len(nums1_[maxIndex1+1:]), len(nums2_), k_-1) not in dp:
-					stack.append((len(nums1_[maxIndex1+1:]), len(nums2_), k_-1))
-					needContinue = True
-				if (len(nums1_), len(nums2_[maxIndex2+1:]), k_-1) not in dp:
-					stack.append((len(nums1_), len(nums2_[maxIndex2+1:]), k_-1))
-					needContinue = True
-				if needContinue:
-					continue
-				r = max(dp[(len(nums1_[maxIndex1+1:]), len(nums2_), k_-1)], dp[(len(nums1_), len(nums2_[maxIndex2+1:]), k_-1)])
-				r.insert(0, maxValue1)
-			elif maxValue1 > maxValue2:
-				if (len(nums1_[maxIndex1+1:]), len(nums2_), k_-1) not in dp:
-					stack.append((len(nums1_[maxIndex1+1:]), len(nums2_), k_-1))
-					continue
-				r = dp[(len(nums1_[maxIndex1+1:]), len(nums2_), k_-1)]
-				r.insert(0,maxValue1)
+	def merge(self, nums1, nums2):
+		result = []
+		while len(nums1) + len(nums2) > 0:
+			if nums1 > nums2:
+				result.append(nums1[0])
+				nums1 = nums1[1:]
 			else:
-				if (len(nums1_), len(nums2_[maxIndex2+1:]), k_-1) not in dp:
-					stack.append((len(nums1_), len(nums2_[maxIndex2+1:]), k_-1))
-					continue
-				r = dp[(len(nums1_), len(nums2_[maxIndex2+1:]), k_-1)]
-				r.insert(0, maxValue2)
-			stack.pop()
-			#print "ahahah", n1, n2, k_, len(nums1_), len(nums2_)
-			dp[(len(nums1_), len(nums2_), k_)] = r
-		return dp[(len(nums1), len(nums2), k)]
-
-	# Find the index of max value in nums[:k]. If there are multiple max value, we return the first index.
-	def findMax(self, nums, k):
-		if len(nums) == 0:
-			return None
-
-		maxIndex = 0
-		for i in range(1, min(k, len(nums))):
-			if nums[i] > nums[maxIndex]:
-				maxIndex = i
-		return maxIndex
+				result.append(nums2[0])
+				nums2 = nums2[1:]
+		return result
 
 
 if __name__ == '__main__':
 
-	print Solution().maxNumber([1,2,0,1,1,2,1,0,2,0,2,1,1,1,1,0,0,2,0,0,2,1,2,1,0,1,1,0,1,2,1,0,2,0,0,1,0,1,2,0,0,0,1,1,2,1,1,1,0,0,0,0,1,2,1,2,1,1,1,2,2,2,1,2,2,0,2,0,0,0,2,1,0,2,2,0,0,2,2,2,0,2,2,2,1,1,0,0,2,1,1,1,0,1,1,1,1,2,1,0,0,1,1,1,2,1,1,1,0,0,0,2,2,2,1,2,0,0,1,1,2,1,1,1,1,1,0,2,2,2,0,2,2,0,1,1,1,1,2,0,2,2,1,0,2,0,1,0,1,2,0,0,0,0,2,2,2,2,2,1,1,0,1,2,2,2,1,2,0,0,1,2,2,1,1,1,1,0,1,0,1,1,0,0,2,2,1,2,1,2,0,0,2,0,2,1,1,1,2,0,0,1,1,1,2,1,1,1,2,2,1,2,2,2,0,0,2,2,0,1,0,0,0,1,1,2,2,2,0,0,1,2,0,0,2,2,1,0,2,1,0,0,0,0,1,0,2,1,0,1,2,1,1,0,1,2,1,2,0,1,1,2,1,1,0,1,0,1,1,1,1,1,0,1,0,0,0,1,2,0,2,2,0,2,1,0,2,1,0,0,0,2,2,2,0,1,1,1,2,0,2,1,0,2,2,0,1,1,0,1,2,2,0,2,2,0,1,0,2,0,1,1,1,0,1,2,2,0,1,0,2,0,2,0,2,0,2,0,2,2,1,1,0,0,0,2,2,1,1,2,1,2,0,2,0,1,0,1,1,0,0,0,2,1,0,0,1,1,0,1,0,0,1,1,0,1,1,2,0,0,0,1,1,2,2,2,2,2,1,1,1,0,0,1,0,0,2,1,2,2],
-							   [2,1,2,1,0,0,0,1,2,1,0,1,1,2,1,2,1,1,2,0,0,0,1,0,1,0,2,0,1,2,0,2,0,1,2,0,1,0,0,0,1,0,1,1,1,2,2,2,1,2,1,0,2,0,0,2,0,2,0,1,2,1,0,1,1,2,0,0,2,0,2,2,2,2,1,2,1,2,1,1,0,0,1,2,2,2,1,1,1,1,1,0,1,0,0,1,1,1,0,0,2,0,1,1,1,2,1,1,0,0,0,0,1,1,1,2,0,2,2,2,0,2,2,0,1,2,0,1,0,0,1,1,0,1,2,0,1,1,2,2,1,1,0,2,1,0,2,2,0,1,2,1,1,2,1,0,2,2,1,1,1,1,1,2,1,1,0,2,0,2,2,0,2,1,0,0,1,0,0,0,2,2,2,0,2,2,0,0,1,1,1,0,0,2,2,1,1,2,0,1,0,2,1,1,1,0,1,2,0,2,2,1,1,0,0,2,1,1,2,1,1,0,1,1,0,2,1,0,2,2,2,1,0,1,2,1,0,2,1,1,1,1,1,2,1,0,2,1,0,2,2,2,1,1,2,1,1,1,2,2,0,0,2,0,0,0,2,2,2,1,2,2,2,2,2,0,1,1,0,2,1,2,2,2,1,0,2,2,2,0,1,1,0,1,1,0,2,1,1,2,1,0,2,2,1,2,2,0,1,1,2,1,0,0,2,2,2,1,2,1,0,1,1,2,0,2,0,0,2,0,2,0,0,1,0,0,1,0,1,0,2,2,1,1,2,0,2,1,2,0,1,2,1,0,0,2,1,2,2,1,0,0,2,2,1,2,2,1,1,2,0,2,1,2,2,1,0,1,2,0,1,0,1,1,0,2,0,1,2,2,1,2,0,1,2,0,1,1,0,2],
-							   800)
+	print Solution().maxNumber([1,2],[],2)
+
+	
