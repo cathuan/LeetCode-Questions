5e833308f0dca5419f4f4bc9e377d0cf0203c422
cathuan <geli7630@gmail.com>
2018-11-04 00:50:47 +1100

    Finish q684.py

---
+++ q684.py
@@ -0,0 +1,35 @@
+# Disjointed-Set Union
+class DSU(object):
+
+    def __init__(self):
+        self.parents = range(1001)
+        self.ranks = [0] * 1001
+
+    def query(self, x):
+        if self.parents[x] != x:
+            self.parents[x] = self.query(self.parents[x])
+        return self.parents[x]
+
+    def connect(self, x, y):
+        xr, yr = self.query(x), self.query(y)
+        if xr == yr:
+            return False
+        elif self.ranks[xr] < self.ranks[yr]:
+            self.parents[xr] = yr
+        elif self.ranks[xr] > self.ranks[yr]:
+            self.parents[yr] = xr
+        else:
+            self.parents[yr] = xr
+            self.ranks[xr] += 1
+        return True
+
+
+class Solution(object):
+
+    def findRedundantConnection(self, edges):
+        dsu = DSU()
+        for edge in edges:
+            x, y = edge
+            is_connected = dsu.connect(x, y)
+            if not is_connected:
+                return edge
