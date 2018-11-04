aaa1e601957eee734874b39dd37bfd100cfbf87e
cathuan <geli7630@gmail.com>
2018-11-05 00:21:00 +1100

    start

---
+++ q381.py
@@ -0,0 +1,29 @@
+class RandomizedCollection(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        
+
+    def insert(self, val):
+        """
+        Inserts a value to the collection. Returns true if the collection did not already contain the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        
+
+    def remove(self, val):
+        """
+        Removes a value from the collection. Returns true if the collection contained the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        
+
+    def getRandom(self):
+        """
+        Get a random element from the collection.
+        :rtype: int
+        """
