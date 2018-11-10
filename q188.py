63db078a6484ac673a4897769a9c21cdd4634967
cathuan <geli7630@gmail.com>
2018-11-10 19:48:31 +1100

    Not optimal solution. Modify q188 comments.

--- q188.py
+++ q188.py
@@ -1,3 +1,7 @@
+# Some solution/explanation:
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/discuss/135704/Detail-explanation-of-DP-solution
+
+
 class Solution(object):
     def maxProfit(self, k, prices):
         """
@@ -9,11 +13,17 @@ class Solution(object):
         if k >= len(prices)/2:
             return self.quickSolve(prices)
 
+        # here the memory is not so efficient.
+        # should use O(n) instead of O(kn)
         dp = []
         for _ in range(k+1):
             dp.append([0]*len(prices))
 
         # dp[i][j] = maximum pnl for at most i trades up to day j
+        # dp[0][j] = 0
+        # dp[i][j] =
+        #   max(dp[i][j-1],  # not trade on the day i
+        #       dp[i-1][k-1] + prices[j] - prices[k] for k = 0..j-1)  # last trade on day k to j
         for i in range(1, k+1):
             lastTrade = prices[0]
             for j in range(1, len(prices)):
@@ -22,6 +32,9 @@ class Solution(object):
 
         return dp[k][len(prices)-1]
 
+    # when k >= len(prices)/2, then maximize profits since the number of
+    # trading opportunities is sufficient. This is faster and avoid
+    # memory usage when k is large.
     def quickSolve(self, prices):
         profit = 0
         for i in range(1, len(prices)):
