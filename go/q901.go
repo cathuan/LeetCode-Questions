4404d9aba4d3ae0c753ccbc4f0df8f2e1484c1ce
cathuan <geli7630@gmail.com>
2018-11-16 03:58:57 +1100

    Finish with go q901

---
+++ go/q901.go
@@ -0,0 +1,45 @@
+package main
+
+type StockSpanner struct {
+	count   int
+	prices  []int
+	indices []int
+}
+
+func Constructor() StockSpanner {
+	span := new(StockSpanner)
+	span.count = 1
+	return *span
+}
+
+func (span *StockSpanner) Next(price int) int {
+	for len(span.prices) > 0 {
+		prevPrice := span.prices[len(span.prices)-1]
+		if prevPrice <= price {
+			span.prices = span.prices[:len(span.prices)-1]
+			span.indices = span.indices[:len(span.indices)-1]
+		} else {
+			break
+		}
+	}
+
+	var days int
+	var preCount int
+	if len(span.prices) == 0 {
+		days = span.count
+	} else {
+		preCount = span.indices[len(span.indices)-1]
+		days = span.count - preCount
+	}
+
+	span.prices = append(span.prices, price)
+	span.indices = append(span.indices, span.count)
+	span.count += 1
+	return days
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Next(price);
+ */
