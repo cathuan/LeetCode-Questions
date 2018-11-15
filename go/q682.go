023d549a74ce3ef82d3c593ae8800cf434b03a93
cathuan <geli7630@gmail.com>
2018-11-15 18:12:33 +1100

    Finish q682 with go

---
+++ go/q682.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func calPoints(ops []string) int {
+	var scores []int
+
+	for _, op := range ops {
+		if op == "C" {
+			scores = scores[:len(scores)-1]
+		} else if op == "D" {
+			prevValidScore := scores[len(scores)-1]
+			scores = append(scores, 2*prevValidScore)
+		} else if op == "+" {
+			prevValidScore1 := scores[len(scores)-1]
+			prevValidScore2 := scores[len(scores)-2]
+			scores = append(scores, prevValidScore1+prevValidScore2)
+		} else {
+			i, _ := strconv.Atoi(op)
+			scores = append(scores, i)
+		}
+	}
+
+	score := 0
+	for _, s := range scores {
+		score = score + s
+	}
+
+	return score
+}
+
+func main() {
+	ops := []string{"5", "-2", "4", "C", "D", "9", "+", "+"}
+	score := calPoints(ops)
+	fmt.Println(score)
+}
