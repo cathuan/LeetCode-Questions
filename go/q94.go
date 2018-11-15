59ee3a8dd427ce3fb64166f8177d1278365266a4
cathuan <geli7630@gmail.com>
2018-11-16 01:23:09 +1100

    Modify file

--- go/q94.go
+++ go/q94.go
@@ -1,11 +1,6 @@
 package main
 
 // TreeNode Definition for a binary tree node.
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
 
 func inorderTraversal(root *TreeNode) []int {
 	var res []int
