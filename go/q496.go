1f437f33e757f92553bf397243341bb1d7928b97
cathuan <geli7630@gmail.com>
2018-11-15 18:37:17 +1100

    Finish q496 with go

---
+++ go/q496.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func nextGreaterElement(findNums []int, nums []int) []int {
+	var stack []int
+	buf := make(map[int]int)
+
+	for _, num := range nums {
+		// empty stack
+		if len(stack) == 0 {
+			stack = append(stack, num)
+		} else {
+			// pop extra numbers out of stack
+			for (len(stack) > 0) && (stack[len(stack)-1] < num) {
+				// pop the last element from the stack
+				key := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				buf[key] = num
+			}
+			stack = append(stack, num)
+		}
+	}
+
+	for len(stack) > 0 {
+		key := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		buf[key] = -1
+	}
+
+	var ret []int
+	for _, num := range findNums {
+		ret = append(ret, buf[num])
+	}
+
+	return ret
+}
+
+func main() {
+	nums1 := []int{4, 1, 2}
+	nums2 := []int{1, 3, 4, 2}
+	ret := nextGreaterElement(nums1, nums2)
+	fmt.Println(ret)
+}
