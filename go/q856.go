5057a00ab5bc310bf5a1221088e066b47f82191b
cathuan <geli7630@gmail.com>
2018-11-15 23:37:34 +1100

    Finish q856 with go

---
+++ go/q856.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func scoreOfParentheses(S string) int {
+	var bracketStack []string
+	var scoreStack []int
+	total := 0
+	for _, c := range S {
+		char := string(c)
+		//fmt.Println(char, scoreStack, bracketStack)
+		if char == "(" {
+			bracketStack = append(bracketStack, string(c))
+			for len(scoreStack) < len(bracketStack) {
+				scoreStack = append(scoreStack, 0)
+			}
+		} else {
+			// get ()
+			if (len(bracketStack) > 0) && (bracketStack[len(bracketStack)-1] == "(") {
+				if len(scoreStack) > len(bracketStack) {
+					lastScore := scoreStack[len(scoreStack)-1]
+					scoreStack = scoreStack[:len(scoreStack)-1]
+					scoreStack[len(scoreStack)-1] = scoreStack[len(scoreStack)-1] + lastScore*2
+				} else {
+					scoreStack[len(scoreStack)-1] = scoreStack[len(scoreStack)-1] + 1
+				}
+				bracketStack = bracketStack[:len(bracketStack)-1]
+			}
+		}
+
+		if (len(scoreStack) == 1) && (len(bracketStack) == 0) {
+			total = total + scoreStack[0]
+			scoreStack = []int{}
+		}
+	}
+	for i := 0; i < len(scoreStack); i = i + 1 {
+		total = total + scoreStack[i]
+	}
+	return total
+}
+
+func main() {
+	s := "(()(())"
+	ret := scoreOfParentheses(s)
+	fmt.Println(ret)
+}
