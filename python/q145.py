8fdb086c175e5945d77e29dff2a088022094d29f
cathuan <geli7630@gmail.com>
2018-11-16 21:52:28 +1100

    Finish q145

---
+++ python/q145.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+
+        if not root:
+            return []
+        
+        stack = []
+        ret = []
+
+        stack.append(root)
+        while stack:
+            node = stack.pop()
+            ret.append(node.val)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        
+        ret.reverse()
+        return ret
+        
