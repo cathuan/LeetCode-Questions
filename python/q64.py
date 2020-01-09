e4b87aa22af2d35748f42eee3e8b4757001c20e0
cathuan <geli7630@gmail.com>
2020-01-10 01:43:20 +1100

    speed up q64

--- python/q64.py
+++ python/q64.py
@@ -1,10 +1,6 @@
-import numpy as np
-
-
 class Solution(object):
 
-    @profile
-    def minPathSum(self, grid):
+    def minPathSum(self, grid) -> int:
         """
         :type grid: List[List[int]]
         :rtype: int
@@ -17,42 +13,20 @@ class Solution(object):
         if nCols == 0:
             return 0
 
-        matrix = []
-        for i in range(nRows):
-            row = []
-            for j in range(nCols):
-                row.append(-1)
-            matrix.append(row)
-
-        matrix[nRows-1][nCols-1] = grid[nRows-1][nCols-1]
-
         for i in range(nRows-1, -1, -1):
             for j in range(nCols-1, -1, -1):
-                if i == nRows-1 and j == nCols - 1:
-                    matrix[i][j] = grid[i][j]
-                    continue
-                right_value = self.test_right(i, j, nRows, nCols, grid, matrix)
-                down_value = self.test_down(i, j, nRows, nCols, grid, matrix)
-                matrix[i][j] = min(right_value, down_value)
+                if i < nRows - 1:
+                    value = min(grid[i][j] + grid[i][j+1], grid[i][j] + grid[i+1][j]) if j < nCols - 1 \
+                        else grid[i][j] + grid[i+1][j]
+                else:
+                    value = grid[i][j] + grid[i][j+1] if j < nCols - 1 else grid[i][j]
+                
+                grid[i][j] = value
 
-        return matrix[0][0]
-
-    def test_down(self, i, j, nRows, nCols, grid, matrix):
-
-        if i == nRows - 1:
-            return np.inf
-        else:
-            return grid[i][j] + matrix[i+1][j]
-
-    def test_right(self, i, j, nRows, nCols, grid, matrix):
-
-        if j == nCols - 1:
-            return np.inf
-        else:
-            return grid[i][j] + matrix[i][j+1]
+        return grid[0][0]
 
 
 if __name__ == "__main__":
 
     grid = [[1]*100]*100
-    print Solution().minPathSum(grid)
+    print(Solution().minPathSum(grid))
