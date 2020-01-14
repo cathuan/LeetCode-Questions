162e45670a0d653b7a22c841c85aa72898409884
cathuan <geli7630@gmail.com>
2020-01-14 23:50:55 +1100

    solve q39

--- python/q39.py
+++ python/q39.py
@@ -1,28 +1,37 @@
-# FIXME: Unfinished at all.
+from typing import List
 
 
 class Solution:
 
-    def __init__(self):
-        self.stack = []
-        self.results = {}
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        # trivial case
+        if candidates == []:
+            return []
 
-    def combinationSum(self, candidates, target: int):
-        self.stack.append(target)
-        candidates = sorted(candidates)
-        while len(self.stack) > 0:
-            self.expand(candidates)
-
-        return self.results[target]
-
-    def expand(self, candidates):
-        v = self.stack[-1]
-        if v not in self.results:
-            self.results[v] = []
+        d = {}
+		
         for candidate in candidates:
-            if candidate > v:
-                continue
-            pass
+            for i in range(1, target+1):
+                if i == candidate:
+                    if i not in d:
+                        d[i] = [[candidate]]
+                    else:
+                        d[i] += [[candidate]]
+						
+				# see if i - candidate  exists in dicationary already
+                elif i - candidate > 0 and i - candidate in d:
+                    for num_set in d[i - candidate]:
+						# add candidate to the saved sets in d[i-candidate]
+                        x = num_set + [candidate]
+                        if i not in d:
+                            d[i] = [x]
+                        else:
+                            d[i].append(x)
+
+        if target not in d:
+            return []
+        else:
+            return d[target]
 
 
 if __name__ == "__main__":
